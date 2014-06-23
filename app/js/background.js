6128605da40314400b403a5035b17d1fa690bc6d
ksomeya <kntsmy@hotmail.co.jp>
2014-06-23 20:09:11 +0900

    headerからレスポンスがmsgpackであるかの判断をできるようにした

--- app/js/background.js
+++ app/js/background.js
@@ -5,3 +5,40 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     });
     chrome.pageAction.show(tabId);
 });
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details){
+        return {"requestHeaders": details.requestHeaders};
+    },
+    {
+        urls: [
+            "http://*/*"
+        ],
+    },
+    [
+        "requestHeaders",
+        "blocking"
+    ]
+);
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details){
+        var isMsgpack = false;
+        details.responseHeaders.forEach(function(header) {
+            if (header.name === "Content-Type" && header.value === "application/x-msgpack") {
+                isMsgpack = true;
+            }
+        });
+        console.log("isMsgpack:", isMsgpack);
+        return {"responseHeaders": details.responseHeaders};
+    },
+    {
+        urls: [
+            "http://*/*"
+        ]
+    },
+    [
+        "responseHeaders",
+        "blocking"
+    ]
+);
